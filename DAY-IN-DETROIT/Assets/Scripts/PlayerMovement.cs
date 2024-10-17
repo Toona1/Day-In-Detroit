1e44a35c50f3cd0e25eb8076e28bf176f04fc012
TheMobiL <124905245+TheMobiL@users.noreply.github.com>
2024-10-17 13:07:24 -0400

    player collision + rudimentary dash
    collision still a bit iffy (it does the thing where you go into the wall) but it works for now

--- DAY-IN-DETROIT/Assets/Scripts/PlayerMovement.cs
+++ DAY-IN-DETROIT/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed; // no value assigned as you can do that in the unity object
+    bool dashing = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,23 +19,37 @@ public class PlayerMovement : MonoBehaviour
     {
         Rigidbody2D rigid = GetComponent<Rigidbody2D>(); // unused atm but i'm leaving it in
         Vector2 pos = transform.position;
-        if (Input.GetKey("a"))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             pos.x -= speed * Time.deltaTime;
         }
-        if (Input.GetKey("d"))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             pos.x += speed * Time.deltaTime;
         }
-        if (Input.GetKey("w"))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             pos.y += speed * Time.deltaTime;
         }
-        if (Input.GetKey("s"))  
+        if (Input.GetKey(KeyCode.DownArrow))  
         {
             pos.y -= speed * Time.deltaTime;
         }
+        if (Input.GetKeyDown(KeyCode.S) & dashing == false) // have to use keydown here because getkey runs every single frame
+        {
+            StartCoroutine(Dash(5));
+        }
 
         transform.position = pos;
+    }
+
+    IEnumerator Dash(int speedincrease)
+    {
+        speed += speedincrease;
+        dashing = true;
+        yield return new WaitForSeconds(0.5f);
+        speed -= speedincrease;
+        yield return new WaitForSeconds(0.5f); // cooldown on the dash
+        dashing = false;
     }
 }

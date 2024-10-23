4ed5d2b436ac5b5387445d41645c2a2fcb4bd05f
TheMobiL <124905245+TheMobiL@users.noreply.github.com>
2024-10-23 18:13:28 -0400

    added enemy rotation
    might remove this later since i have no idea what the enemies are supposed to look like

--- DAY-IN-DETROIT/Assets/Scripts/EnemyClass.cs
+++ DAY-IN-DETROIT/Assets/Scripts/EnemyClass.cs
@@ -55,8 +55,11 @@ public class EnemyClass : MonoBehaviour // class and class variables
     // Update is called once per frame
     void Update()
     {
-        SetDistance(Vector2.Distance(transform.position, player.transform.position)); // code to make the enemy follow the player
+        SetDistance(Vector2.Distance(transform.position, player.transform.position)); // code to make the enemy follow and turn to the player
         Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, GetSpeed() * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle); // might comment this part out later because i don't know how the art is gonna look so i'm not sure if this is going to work well with it
     }
 }

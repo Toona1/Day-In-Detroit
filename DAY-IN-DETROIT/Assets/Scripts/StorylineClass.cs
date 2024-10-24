fc4d42bdeb66155edd7318860bbf02cc083593ce
ASav123 <144723769+ASav123@users.noreply.github.com>
2024-10-23 22:45:31 -0400

    Fixed getting and setting issue with storyline class

--- DAY-IN-DETROIT/Assets/Scripts/StorylineClass.cs
+++ DAY-IN-DETROIT/Assets/Scripts/StorylineClass.cs
@@ -2,50 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class StorylineClass : MonoBehaviour
 {
     public TextMeshProUGUI text;
     public StorylineController storylineText;
 
-    //Add if scene != main menu
+    //Storyline varibales
     private string _storyText1;
     private string _storyText2;
     private int _delay;
 
+    //Getters and setters for texts and delay
     public void SetText1(string text) {
         this._storyText1 = text;
-        Debug.Log("Text1 made");
+    }
+    public string GetText1() {
+        return this._storyText1;
     }
     public void SetText2(string text) {
         this._storyText2 = text;
-        Debug.Log("Text2 made");
-        Debug.Log(this._storyText2);
+    }
+    public string GetText2() {
+        return this._storyText2;
     }
     public void SetDelay(int delay) {
         this._delay = delay;
     }
-    public void RunStoryLine() {
-        StartCoroutine(TextOutputAndDelay());
-        Debug.Log("Wokring");
-    }
 
-    // Make this into a function that runs when the controller tells it to
-    void Start()
+    //Coroutines regulating the output of text
+    public IEnumerator TextOutput(string storyText)
     {
-        this._storyText1 = "You are Dale, Dale lives in Detroit, Dale needs to get home from work with all the money he made. Simple..";
-        this._storyText2 = "Lester is calling.\r\n“The IRS will be at your place soon, destroy the evidence”...\r\nDave needs to get home quickly..\r\n";
-
-        Debug.Log("Class running");
-        text.text = " ";
-        Debug.Log("Class running");
-        StartCoroutine(TextOutputAndDelay());
-    }
-
-    //this._delay = 2;
-    
-    public IEnumerator TextOutput(string storyText){
-        for (int i = 0; i < storyText.Length; i++) {
+        for (int i = 0; i < storyText.Length; i++)
+        {
             yield return new WaitForSeconds(0.05f);
             text.text = storyText.Substring(0, i);
         }
@@ -53,7 +43,8 @@ public class StorylineClass : MonoBehaviour
         text.text = "";
 
     }
-    public IEnumerator TextSecDelay(int delay) {
+    public IEnumerator TextSecDelay(int delay)
+    {
         yield return new WaitForSeconds(delay);
     }
     public IEnumerator TextOutputAndDelay()
@@ -63,4 +54,21 @@ public class StorylineClass : MonoBehaviour
         yield return StartCoroutine(TextOutput(this._storyText2));
         yield return StartCoroutine(TextSecDelay(this._delay));
     }
+
+    //Gets and sets values 
+    void Start()
+    {
+        storylineText = GetComponent<StorylineController>();
+        SetText1(storylineText.GetText1());
+        SetText2(storylineText.GetText2());
+        SetDelay(storylineText.GetDelay());
+
+        Debug.Log("Class running");
+        text.text = " ";
+        Debug.Log("Class running");
+        StartCoroutine(TextOutputAndDelay());
+    }
+
+
+
 }
--- DAY-IN-DETROIT/Assets/Scripts/StorylineController.cs
+++ DAY-IN-DETROIT/Assets/Scripts/StorylineController.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StorylineController : MonoBehaviour
 {
+    public TextMeshProUGUI text;
     public StorylineClass storyline;
-    // Start is called before the first frame update
+
+    //Storyline variables 
+    private string _text1 = "You are Dale, Dale lives in Detroit, Dale needs to get home from work with all the money he made. Simple.";
+    private string _text2 = "Lester is calling.'The IRS will be at your place soon, destroy the evidence'...Dave needs to get home quickly...";
+    private int _delay = 5;
+
+
+    //Getters for storyline information
+    public string GetText1() {
+        return this._text1;
+    }
+    public string GetText2() {
+        return this._text2;
+    }
+    public int GetDelay() {
+        return this._delay;
+    }
+
+
     void Start()
     {
         storyline = GetComponent<StorylineClass>();
-        storyline.SetText1("You are Dale, Dale lives in Detroit, Dale needs to get home from work with all the money he made. Simple..");
-        storyline.SetText2("Lester is calling.\r\n“The IRS will be at your place soon, destroy the evidence”...\r\nDave needs to get home quickly..\r\n");
-        storyline.SetDelay(2);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
